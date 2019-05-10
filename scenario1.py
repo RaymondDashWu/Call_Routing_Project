47f00961449cad70dc9dad125a77f60990074fcf
RaymondDashWu <raymond31670@hotmail.com>
2019-05-10 16:33:57 -0700

    [ADD] Scenario 1 done

--- scenario1.py
+++ scenario1.py
@@ -2,24 +2,4 @@
 
 # You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a single phone number. How quickly can you find the cost of calling this number?
 
-with open("route-costs-106000.txt", "r") as f:
-    words = f.read().split("\n")
-
-
-
-
-
-
-def binary_search_recursive(array):
-    return array
-    
-    
-    
-    # """return the index of item in sorted array or None if item is not found"""
-    # # implement binary_search_iterative and binary_search_recursive below, then
-    # # change this to call your implementation to verify it passes all tests
-    # array.sort()
-    # # return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
-
-print(binary_search_recursive(words))
+CTRL (or CMD for Mac) + F to look for a single phone number
