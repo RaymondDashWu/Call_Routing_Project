9fe4664eee060608225f1899a2552530f955615d
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-05-10 16:29:54 -0700

    [add] implemented method for scenario 1 & 2

--- scenario1.py
+++ scenario1.py
@@ -7,19 +7,32 @@ with open("route-costs-106000.txt", "r") as f:
 
 
 
+def search_for_cost():
+    route_num = {'+86153':'0.84', '+449275049':'0.49', '+8130':'0.68'}
+    phone_num = '+81301234567'
+    is_match = False
+    # counter = 0
+    prefix = phone_num
+    min_prefix_len = 1
+
+# 8130 compared to 81201234567
+
+while len(prefix) > min_prefix_len:  # O(m) where m is len(phone_num) iterations
+   if prefix in route_num:  # O(l)
+       print(phone_num, route_num[prefix])
+       break
+   else:
+       # counter = len(phone_num) - 1
+       prefix = prefix[:-1]
+
+
+# Provide full  phone # with route
 
 
 
-def binary_search_recursive(array):
-    return array
-    
-    
-    
     # """return the index of item in sorted array or None if item is not found"""
     # # implement binary_search_iterative and binary_search_recursive below, then
     # # change this to call your implementation to verify it passes all tests
     # array.sort()
     # # return binary_search_iterative(array, item)
     # return binary_search_recursive(array, item)
-
-print(binary_search_recursive(words))
