a5cde74837f0521ec9733531be19bfb8a7ee2df1
RaymondDashWu <raymond31670@hotmail.com>
2019-05-10 16:38:03 -0700

    [ADD] scenario1

---
+++ scenario1.py
@@ -0,0 +1,5 @@
+# Scenario 1: One-time route cost check
+
+# You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a single phone number. How quickly can you find the cost of calling this number?
+
+CTRL (or CMD for Mac) + F to look for a single phone number
