a681b47e211af5ac67e1c63eedc8e14501deb5ad
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-05-11 01:52:54 -0700

    [add] implmented challenge 2

--- scenario2.py
+++ scenario2.py
@@ -2,20 +2,21 @@
 # You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a list of 1000 phone numbers. How can you operationalize the route cost lookup problem?
 
 
-# You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a single phone number. How quickly can you find the cost of calling this number?
 phone_num_cost = {}
+
 with open("route-costs-100.txt", "r") as f:
     for line in f:
         (key, val) = line.strip('\n').split(',')
         phone_num_cost[key] = val
 
+with open("phone-numbers-100.txt", "r") as f:
+    phone_num = f.read().split()
 
 
 def search_for_cost(dictionary, phone_num):
 
     prefix = phone_num
     min_prefix_len = 1
-
     while len(prefix) > min_prefix_len:
 
         if prefix in dictionary: # O(m) where m is len(phone_num) iterations
@@ -23,13 +24,21 @@ def search_for_cost(dictionary, phone_num):
         else:
            prefix = prefix[:-1]
 
-def main():
-    phone_num = '+449916707'
-    print(search_for_cost(phone_num_cost, phone_num))
 
+def anotate_data(phone_num_list, dictionary):
+    f = open('route-costs-2.txt', 'w+')
+    for number in phone_num_list:
+        data_cost = search_for_cost(dictionary, number)
+        if data_cost is not None:
+            f.write(data_cost)
+    f.close()
+
+
+
+def main():
+    print(anotate_data(phone_num, phone_num_cost))
 
 
 
 if __name__ == "__main__":
     main()
-    
