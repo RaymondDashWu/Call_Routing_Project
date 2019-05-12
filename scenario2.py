615cc9364cc055736d887f1d5d3457668b3fe8e5
RaymondDashWu <raymond31670@hotmail.com>
2019-05-12 14:42:43 -0700

    [ADD] Time/Space complexity notes + docstring

--- scenario2.py
+++ scenario2.py
@@ -7,11 +7,17 @@ start_read = time.time()
 phone_num_cost = {}
 cell_number_list = {}
 
+# Importing all routes and associated costs and storing them into memory
+# Time Complexity: O(n) to iterate through all lines in route costs file
+# Space Complexity: O(n) to store all all values in route costs file
 with open("route-costs-106000.txt", "r") as f:
     for line in f:
         (key, val) = line.strip('\n').split(',')
         phone_num_cost[key] = val
 
+# Importing all the phone numbers we need to find costs for
+# Time Complexity: O(n) to iterate through all lines in phone-numbers file
+# Space Complexity: O(n) to store all n values in phone number file
 with open("phone-numbers-1000.txt", "r") as f:
     for line in f:
         (key, val) = line.split('\n')
@@ -20,6 +26,11 @@ with open("phone-numbers-1000.txt", "r") as f:
 end_read = time.time()
 
 def search_for_cost(dictionary, number):
+    """Searches for the given phone number and returns associated cost
+    Format ex: +1200, .03
+    Time Complexity: O(m) where m is the length of the phone number
+    Space Complexity: O(l) to store the length of the phone number. Decreases until
+    a match is found"""
     prefix = number
     min_prefix_len = 1
     while len(prefix) > min_prefix_len:
@@ -34,6 +45,9 @@ def search_for_cost(dictionary, number):
         return '{}, {}\n'.format(number, 0)
 
 def anotate_data(phone_num_list, dictionary):
+    """Writes the phone number and associated cost to a file
+    Time Complexity: O(n) to iterate through all keys in phone_num_list dictionary
+    Space Complexity: O(1) this function does not store anything in memory."""
     file = open('route-costs-2.txt', 'w+')
     for number in phone_num_list.keys():
         data_cost = search_for_cost(dictionary, number)
--- scenario3.py
+++ scenario3.py
@@ -9,15 +9,22 @@ start_read = time.time()
 phone_num_cost = {}
 cell_number_list = {}
 
-all_route_costs = glob.glob(os.path.join('', "route-costs-*.txt"))
 
-# print("all_route_costs", all_route_costs)
+# Importing all routes and associated costs and storing them into memory
+# Time Complexity: O(n*m) to iterate through m route cost files with each one containing
+# n lines
+# Space Complexity: O(n*m) to store all n values of all m route costs files
+all_route_costs = glob.glob(os.path.join('', "route-costs-*.txt"))
 for route in all_route_costs:
     with open(route, "r") as f:
         for line in f:
             (key, val) = line.strip('\n').split(',')
             phone_num_cost[key] = val
 
+# Importing all the phone numbers we need to find costs for
+# Time Complexity: O(n*m) to iterate through m phone number files with each one containing
+# n lines
+# Space Complexity: O(n*m) to store all n values of all m phone number files
 with open("phone-numbers-10000.txt", "r") as f:
     for line in f:
         (key, val) = line.split('\n')
@@ -26,20 +33,27 @@ with open("phone-numbers-10000.txt", "r") as f:
 end_read = time.time()
 
 def search_for_cost(dictionary, number):
+    """Searches for the given phone number and returns associated cost
+    Format ex: +1200, .03
+    Time Complexity: O(m) where m is the length of the phone number
+    Space Complexity: O(l) to store the length of the phone number. Decreases until
+    a match is found"""
     prefix = number
     min_prefix_len = 1
     while len(prefix) > min_prefix_len:
         if prefix in dictionary: # O(m) where m is len(phone_num) iterations
+            # TODO: Find lowest price for numbers
             result = '{}, {}\n'.format(number, dictionary[prefix])
             return result # O(l)
-        # if prefix not in dictionary:
-        #     return '{}, {}\n'.format(number, 0)
         else:
            prefix = prefix[:-1]
     if prefix not in dictionary:
         return '{}, {}\n'.format(number, 0)
 
 def anotate_data(phone_num_list, dictionary):
+    """Writes the phone number and associated cost to a file
+    Time Complexity: O(n) to iterate through all keys in phone_num_list dictionary
+    Space Complexity: O(1) this function does not store anything in memory."""
     file = open('route-costs-3.txt', 'w+')
     for number in phone_num_list.keys():
         data_cost = search_for_cost(dictionary, number)
