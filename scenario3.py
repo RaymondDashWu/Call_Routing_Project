1f3419349f467186542627875f65119ddd72584b
RaymondDashWu <raymond31670@hotmail.com>
2019-05-11 22:59:44 -0700

    [FIX] Removed mmap Scenario 2/3

--- scenario3.py
+++ scenario3.py
@@ -1,7 +1,6 @@
 # Scenario 3: Multiple long carrier route lists
 # You have 5 carrier route lists, each with 10,000,000 (10M) entries (in arbitrary order) and a list of 10,000 phone numbers. How can you speed up your route cost lookup solution to handle this larger dataset?
 import time
-import mmap
 import glob
 import os
 
